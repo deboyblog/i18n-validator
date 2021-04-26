ea0b86f815d076a20aaabfb130942ad5a49556e2
Deboy <55631825@qq.com>
2021-04-26 11:53:31 +0800

    fix: 修复参数无法正常传递的bug

--- bin/i18n-validator.js
+++ bin/i18n-validator.js
@@ -4,10 +4,10 @@ const validator = require('../src/validator');
 program
     .version(require('../package.json').version)
     .description('用于检查翻译结果是否符合源字符的规范，比如是否正确包含html标签或者变量名称是否正确等')
-    .option('-t, --target <path>', 'target dir: ./src/lang')
-    .option('-s, --source', 'target language: zh-cn, en-us', 'zh-cn')
-    .option('-f, --format', 'i18n file format: json,js,ts', 'json')
+    .option('-t, --target <path>', 'target dir eg: ./src/lang')
+    .option('-s, --source [type]', 'target language eg: zh-cn, zh-chs', 'zh-cn')
+    .option('-f, --format', 'i18n file format eg: json,js,ts', 'json')
     .action(function () {
-        validator(program.target, program.source)
+        validator(program.target, program.source, program.format)
     });
 program.parse(process.argv);
